daf0a32d55c0cd9717bd1cb6497c1db2bb3a6fea
Kiwiiil <rizkydwis980@gmail.com>
2024-12-09 21:15:43 +0700

    Update Project.cpp

--- Project.cpp
+++ Project.cpp
@@ -5,21 +5,21 @@
 using namespace std;
 
 void Loding() {
-const char *loading_msg = "Loading, please wait...";
-    const char *animation = "|/-\\";
-    int animation_length = 4;
-    int x = (COLS - strlen(loading_msg)) / 2;
-    int y = LINES / 2;
-
-    for (int i = 0; i < 20; ++i) {
-        clear();
-        attron(A_BOLD | A_BLINK);
-        mvprintw(y - 1, x, "%s", loading_msg);
-        attroff(A_BOLD | A_BLINK);
-        mvwprintw(stdscr, y, x + strlen(loading_msg) + 1, "%c", animation[i % animation_length]);
-        refresh();
-        usleep(100000);
-    }
+	const char *loading_msg = "Loading, please wait...";
+	const char *animation = "|/-\\";
+	int animation_length = 4;
+	int x = (COLS - strlen(loading_msg)) / 2;
+	int y = LINES / 2;
+	
+	for (int i = 0; i < 20; ++i) {
+		clear();
+		attron(A_BOLD | A_BLINK);
+		mvprintw(y - 1, x, "%s", loading_msg);
+		attroff(A_BOLD | A_BLINK);
+		mvwprintw(stdscr, y, x + strlen(loading_msg) + 1, "%c", animation[i % animation_length]);
+		refresh();
+		usleep(100000);
+	}
     clear();
 }
 
@@ -28,41 +28,49 @@ void Menu() {
         "1. Start",
         "2. Exit"
     };
-    int n_choices = sizeof(choices) / sizeof(choices[0]);
-    int highlight = 0;
-    int c;
-    box(stdscr, 0, 0);
+	int n_choices = sizeof(choices) / sizeof(choices[0]);
+	int highlight = 0;
+	int c;
+	int height = 6;
+	int width = 20;
+	int start_y = (LINES - height) / 2;
+	int start_x = (COLS - width) / 2;
+	
+	WINDOW *menu_win = newwin(height, width, start_y, start_x);
+	box(menu_win, 0, 0);
+	mvwprintw(menu_win, 0, 1, "[ MENU OPTIONS ]");
+	refresh();
+	wrefresh(menu_win);
 
-    while (1) {
-    
-        for (int i = 0; i < n_choices; i++) {
-            if (i == highlight) {
-                attron(A_REVERSE);
-                mvprintw(i + 14, 56, "%s", choices[i]);
-                attroff(A_REVERSE);
-            } else {
-                mvprintw(i + 14, 56, "%s", choices[i]);
-            }
-        }
-        refresh();
+	while (1) {
+		for (int i = 0; i < n_choices; i++) {
+	            if (i == highlight) {
+	                attron(A_REVERSE);
+	                mvprintw(i + 14, 55, "%s", choices[i]);
+	                attroff(A_REVERSE);
+	            } else {
+	                mvprintw(i + 14, 55, "%s", choices[i]);
+	            }
+	        }
+	        refresh();
         
-        c = getch();
-        if (c == KEY_UP) {
-            highlight = (highlight == 0) ? n_choices - 1 : highlight - 1;
-        } else if (c == KEY_DOWN) {
-            highlight = (highlight == n_choices - 1) ? 0 : highlight + 1;
-        } else if (c == 10) {
-            break;
-        }
-    }
-    clear();
+	        c = getch();
+	        if (c == KEY_UP) {
+	            highlight = (highlight == 0) ? n_choices - 1 : highlight - 1;
+	        } else if (c == KEY_DOWN) {
+	            highlight = (highlight == n_choices - 1) ? 0 : highlight + 1;
+	        } else if (c == 10) {
+	            break;
+	        }
+	    }
+	    clear();
 }
 
 int main() {
 	initscr();
-	clear();
-    noecho();
-    cbreak();
+	noecho();
+	cbreak();
+	keypad(stdscr, TRUE);
 	
 	Loding();
 	Menu();
