9d3a83629fd679762cc9066ec2d11fe63f7466b7
Fahmiisma <twotwelvetwothousandfour@gmail.com>
2024-12-09 19:32:06 +0700

    Update Project.cpp

--- Project.cpp
+++ Project.cpp
@@ -23,10 +23,49 @@ const char *loading_msg = "Loading, please wait...";
     clear();
 }
 
+void Menu() {
+	const char *choices[] = {
+        "1. Start",
+        "2. Exit"
+    };
+    int n_choices = sizeof(choices) / sizeof(choices[0]);
+    int highlight = 0;
+    int c;
+    box(stdscr, 0, 0);
+
+    while (1) {
+    
+        for (int i = 0; i < n_choices; i++) {
+            if (i == highlight) {
+                attron(A_REVERSE);
+                mvprintw(i + 14, 56, "%s", choices[i]);
+                attroff(A_REVERSE);
+            } else {
+                mvprintw(i + 14, 56, "%s", choices[i]);
+            }
+        }
+        refresh();
+        
+        c = getch();
+        if (c == KEY_UP) {
+            highlight = (highlight == 0) ? n_choices - 1 : highlight - 1;
+        } else if (c == KEY_DOWN) {
+            highlight = (highlight == n_choices - 1) ? 0 : highlight + 1;
+        } else if (c == 10) {
+            break;
+        }
+    }
+    clear();
+}
+
 int main() {
 	initscr();
+	clear();
+    noecho();
+    cbreak();
 	
 	Loding();
+	Menu();
 
     refresh();
     endwin();
